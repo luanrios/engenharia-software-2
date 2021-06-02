b2c732da74bf3c714ac90dcfd6c14612688d4cf2
Pedro Pontes <pontescpedro@gmail.com>
2021-06-02 15:51:06 -0300

    Update app routes

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import Signup from "./Signup";
-import { Container } from "react-bootstrap";
-import { AuthProvider } from "../contexts/AuthContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import Login from "./Login";
-import CreateBook from "./CreateBook";
-import DetailBook from "./DetailBook";
-import EditBook from "./EditBook";
-import PrivateRoute from "./PrivateRoute";
-import ForgotPassword from "./ForgotPassword";
-import UpdateProfile from "./UpdateProfile";
-import ListBooks from "./ListBooks";
+import React from 'react';
+import Signup from './Signup';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import EditProfile from './EditProfile';
+import Login from './Login';
+import CreateBook from './CreateBook';
+import DetailBook from './DetailBook';
+import EditBook from './EditBook';
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
+import ListBooks from './ListBooks';
+import ListBorrowedBooks from './ListBorrowedBooks';
 import TopNavbar from './TopNavbar';
 
 /*
@@ -25,22 +26,29 @@ function App() {
       <TopNavbar />
 
       <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: '100vh' }}
       >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
+        <div className='w-100' style={{ maxWidth: '400px' }}>
           <Router>
             <AuthProvider>
               <Switch>
-                <PrivateRoute exact path="/" component={Dashboard} />
-                <PrivateRoute path="/update-profile" component={UpdateProfile} />
-                <PrivateRoute path="/book/create" component={CreateBook} />
-                <PrivateRoute path="/book/:id/edit" component={EditBook} />
-                <Route path="/books" component={ListBooks} />
-                <Route path="/book/:id" component={DetailBook} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/login" component={Login} />
-                <Route path="/forgot-password" component={ForgotPassword} />
+                <PrivateRoute exact path='/' component={ListBooks} />
+                <PrivateRoute
+                  path='/update-profile'
+                  component={UpdateProfile}
+                />
+                <PrivateRoute
+                  path='/borrowed-books'
+                  component={ListBorrowedBooks}
+                />
+                <PrivateRoute path='/book/create' component={CreateBook} />
+                <PrivateRoute path='/book/:id/edit' component={EditBook} />
+                <PrivateRoute path='/edit-profile' component={EditProfile} />
+                <Route path='/book/:id' component={DetailBook} />
+                <Route path='/signup' component={Signup} />
+                <Route path='/login' component={Login} />
+                <Route path='/forgot-password' component={ForgotPassword} />
               </Switch>
             </AuthProvider>
           </Router>
