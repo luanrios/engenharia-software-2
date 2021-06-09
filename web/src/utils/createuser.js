491748ac3afb7f814aa96584ae17942631b6ef8f
Pedro Pontes <pontescpedro@gmail.com>
2021-06-09 11:33:14 -0300

    Add borrowed books to created user
    Resolves: #7

--- web/src/utils/createuser.js
+++ web/src/utils/createuser.js
@@ -1,27 +1,28 @@
-import firebase from "../firebase";
+import firebase from '../firebase';
 
 /*
     Função de criação de uma instância do usuário
     Importa conexão com o backend de ../firebase
 */
 
-export default async function createUser(uid, email) {
+export default async function createUser(uid, email, borrowedBooks) {
   const database = firebase.firestore();
-  
-  const userCollection = database.collection("users");
-  const libraryCollection = database.collection("libraries");
+
+  const userCollection = database.collection('users');
+  const libraryCollection = database.collection('libraries');
 
   try {
     const newLibraryDoc = await libraryCollection.add({
       user: uid,
-      books: []
+      books: [],
     });
 
     await userCollection.doc(uid).set({
       email,
-      library: newLibraryDoc.id
+      library: newLibraryDoc.id,
+      borrowedBooks,
     });
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 }
